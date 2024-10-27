5af5654cd4e7c02ba561db4fb71dbc4e9c29c108
gonzalez-7 <20agonzalezp@gmail.com>
2024-10-27 12:42:59 -0700

    Completed the first 2 tasks

--- script.js
+++ script.js
@@ -3,9 +3,11 @@
 
 // 1. Write code to output the following string to the console:
 // "Hello World!"
+console.log("Hello World!");
 
 // 2. Write code to concatenate three strings and output the result to the console:
 // "Hello", " ", "World!"
+console.log("Hello" + " " + "World!");
 
 // 3. Write code to output "Hello" on one line and "World!" on the next line to the console.
 // *Note: You should only have one console.log() statement - how do you add a new line in
